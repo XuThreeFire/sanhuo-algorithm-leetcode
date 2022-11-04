9b2cf93cd832b9cfd2849822b5270a18e0465d74
xuthreefire <1040104853@qq.com>
2022-11-04 13:21:17 +0800

    增加lc-322-零钱兑换的数组解

--- 03-LeetCode算法题/01-动态规划/01-LeetCode-322-零钱兑换.go
+++ 03-LeetCode算法题/01-动态规划/01-LeetCode-322-零钱兑换.go
@@ -3,6 +3,14 @@ package main
 var memo = map[int]int{}
 
 func coinChange(coins []int, amount int) int {
+	// 1, 方法1，dp递归，类似后序遍历N叉数 res = min(res, 选择第i棵树)
+	// return coinChange1(coins, amount)
+	// 1, 方法2，dp数组，res = min(不兑换, 兑换第1个硬币, 兑换第2个硬币, 兑换第3个硬币....)
+	return coinChangeDp2(coins, amount)
+
+}
+
+func coinChange1(coins []int, amount int) int {
 	memo = map[int]int{}
 	return dp(coins, amount)
 }
@@ -24,6 +32,9 @@ func dp(coins []int, amount int) int {
 		return v
 	}
 	var res = 1000000
+	// 相当于N叉树的遍历
+	// 遍历：left1, left2, left3, left4, left5, left6
+	// 后序遍历：res = min(res, left)
 	for _, coin := range coins {
 		if amount < coin {
 			continue
@@ -40,6 +51,29 @@ func dp(coins []int, amount int) int {
 	}
 	memo[amount] = res
 	return res
+}
+
+// 动态规划: 自底向上
+// dp[i]：表示金额为i需要最少的币数量，无法兑换，则为Max
+func coinChangeDp2(coins []int, amount int) int {
+	var Max = amount + 1
+	var dp = make([]int, amount+1)
+	dp[0] = 0 //
+	for i := 1; i <= amount; i++ {
+		dp[i] = Max
+		// 遍历全部硬币数量，选择最少的硬币数量
+		// res = min(不兑换, 选择兑换1, 选择兑换2, 选择兑换3, 选择兑换5)
+		for _, coin := range coins {
+			if coin <= i {
+				dp[i] = getMin(dp[i], dp[i-coin]+1) // 兑换次数+1
+			}
+		}
+	}
+	// basecase
+	if dp[amount] == Max {
+		return -1
+	}
+	return dp[amount]
 }
 
 func getMin(a, b int) int {
