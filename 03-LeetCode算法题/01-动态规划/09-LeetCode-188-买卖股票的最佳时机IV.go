fbebb598eb482ceb07a26eb3841a246a635f1f1a
xuthreefire <1040104853@qq.com>
2022-11-16 13:31:22 +0800

    添加188买卖股票最佳时机的题解

--- 03-LeetCode算法题/01-动态规划/09-LeetCode-188-买卖股票的最佳时机IV.go
+++ 03-LeetCode算法题/01-动态规划/09-LeetCode-188-买卖股票的最佳时机IV.go
@@ -11,42 +11,84 @@ func min(a, b int) int {
 	}
 	return b
 }
+
+//func maxProfit188(k int, prices []int) int {
+//	// basecase
+//	n := len(prices)
+//	k = min(k, n/2)
+//	var (
+//		buy = make([]int, k+1)
+//		sal = make([]int, k+1)
+//	)
+//	if n <= 1 {
+//		return 0
+//	}
+//	for i := 1; i <= k; i++ {
+//		buy[i] = math.MinInt / 2
+//		sal[i] = math.MinInt / 2
+//	}
+//
+//	// init buy and sal
+//	buy[0] = -prices[0]
+//	sal[0] = 0
+//	for i := 1; i < n; i++ {
+//		buy[0] = max(buy[0], sal[0]-prices[i])
+//		for j := 1; j <= k; j++ {
+//			buy[j] = max(buy[j], sal[j]-prices[i])
+//			sal[j] = max(sal[j], buy[j-1]+prices[i])
+//		}
+//	}
+//	return sal[k-1]
+//}
+//
+//func max(a, b int) int {
+//	if a > b {
+//		return a
+//	}
+//	return b
+//}
+//
 func maxProfit188(k int, prices []int) int {
-	// basecase
 	n := len(prices)
-	k = min(k, n/2)
-	var (
-		buy = make([]int, k+1)
-		sal = make([]int, k+1)
-	)
-	if n <= 1 {
+	if n == 0 {
 		return 0
 	}
+
+	k = min(k, n/2)
+	buy := make([]int, k+1)
+	sell := make([]int, k+1)
+	buy[0] = -prices[0]
 	for i := 1; i <= k; i++ {
-		buy[i] = math.MinInt / 2
-		sal[i] = math.MinInt / 2
+		buy[i] = math.MinInt64 / 2
+		sell[i] = math.MinInt64 / 2
 	}
 
-	// init buy and sal
-	buy[0] = -prices[0]
-	sal[0] = 0
 	for i := 1; i < n; i++ {
-		buy[0] = max(buy[0], sal[0]-prices[i])
+		buy[0] = max(buy[0], sell[0]-prices[i])
 		for j := 1; j <= k; j++ {
-			buy[j] = max(buy[j], sal[j]-prices[i])
-			sal[j] = max(sal[j], buy[j-1]+prices[i])
+			buy[j] = max(buy[j], sell[j]-prices[i])
+			sell[j] = max(sell[j], buy[j-1]+prices[i])
 		}
 	}
-	return sal[k-1]
+	return max(sell...)
 }
 
-func max(a, b int) int {
-	if a > b {
+func min(a, b int) int {
+	if a < b {
 		return a
 	}
 	return b
 }
 
+func max(a ...int) int {
+	res := a[0]
+	for _, v := range a[1:] {
+		if v > res {
+			res = v
+		}
+	}
+	return res
+}
 func main() {
 	var (
 		k      = 2
