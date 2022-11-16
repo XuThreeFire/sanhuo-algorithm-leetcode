e0f993d95856f7028e43d933188897528612a7f8
xuthreefire <1040104853@qq.com>
2022-11-16 13:32:29 +0800

    fix

--- 03-LeetCode算法题/01-动态规划/09-LeetCode-188-买卖股票的最佳时机IV.go
+++ 03-LeetCode算法题/01-动态规划/09-LeetCode-188-买卖股票的最佳时机IV.go
@@ -12,42 +12,41 @@ func min(a, b int) int {
 	return b
 }
 
-//func maxProfit188(k int, prices []int) int {
-//	// basecase
-//	n := len(prices)
-//	k = min(k, n/2)
-//	var (
-//		buy = make([]int, k+1)
-//		sal = make([]int, k+1)
-//	)
-//	if n <= 1 {
-//		return 0
-//	}
-//	for i := 1; i <= k; i++ {
-//		buy[i] = math.MinInt / 2
-//		sal[i] = math.MinInt / 2
-//	}
-//
-//	// init buy and sal
-//	buy[0] = -prices[0]
-//	sal[0] = 0
-//	for i := 1; i < n; i++ {
-//		buy[0] = max(buy[0], sal[0]-prices[i])
-//		for j := 1; j <= k; j++ {
-//			buy[j] = max(buy[j], sal[j]-prices[i])
-//			sal[j] = max(sal[j], buy[j-1]+prices[i])
+//	func maxProfit188(k int, prices []int) int {
+//		// basecase
+//		n := len(prices)
+//		k = min(k, n/2)
+//		var (
+//			buy = make([]int, k+1)
+//			sal = make([]int, k+1)
+//		)
+//		if n <= 1 {
+//			return 0
 //		}
-//	}
-//	return sal[k-1]
-//}
+//		for i := 1; i <= k; i++ {
+//			buy[i] = math.MinInt / 2
+//			sal[i] = math.MinInt / 2
+//		}
 //
-//func max(a, b int) int {
-//	if a > b {
-//		return a
+//		// init buy and sal
+//		buy[0] = -prices[0]
+//		sal[0] = 0
+//		for i := 1; i < n; i++ {
+//			buy[0] = max(buy[0], sal[0]-prices[i])
+//			for j := 1; j <= k; j++ {
+//				buy[j] = max(buy[j], sal[j]-prices[i])
+//				sal[j] = max(sal[j], buy[j-1]+prices[i])
+//			}
+//		}
+//		return sal[k-1]
 //	}
-//	return b
-//}
 //
+//	func max(a, b int) int {
+//		if a > b {
+//			return a
+//		}
+//		return b
+//	}
 func maxProfit188(k int, prices []int) int {
 	n := len(prices)
 	if n == 0 {
